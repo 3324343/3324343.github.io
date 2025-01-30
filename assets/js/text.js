39691151b95477dc200625b0fb64c0282fcfde10
LilpanXD <146524790+3324343@users.noreply.github.com>
2025-01-31 02:59:43 +0700

    Initial commit

---
+++ assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["You device get hacked!", "boomm!!"],
+        typeSpeed: 10,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
---
+++ assets/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["You device was hacked", "booooom!!!"],
+        typeSpeed: 10,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
